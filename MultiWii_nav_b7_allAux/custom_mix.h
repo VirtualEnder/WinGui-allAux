e5061afeef0f7caf234e709de7b380ed4f9e0a7a
VirtualEnder <virtualender@gmail.com>
2014-08-20 22:59:49 -0400

    adjust for y4 thrust vector

--- MultiWii_nav_b7_allAux/custom_mix.h
+++ MultiWii_nav_b7_allAux/custom_mix.h
@@ -1,5 +1,6 @@
-
+#define VTAILTHRUST sin(0.61)
+// Roll, Pitch, Yaw
 motor[0] = PIDMIX(+0,+1,-1); //REAR_1 CW
-motor[1] = PIDMIX(-1,-1/4, 0); //FRONT_R CCW
+motor[1] = PIDMIX(-1,-1, -VTAILTHRUST); //FRONT_R CCW
 motor[2] = PIDMIX(+0,+1,+1); //REAR_2 CCW
-motor[3] = PIDMIX(+1,-1/4, 0); //FRONT_L CW
+motor[3] = PIDMIX(+1,-1, +VTAILTHRUST); //FRONT_L CW
